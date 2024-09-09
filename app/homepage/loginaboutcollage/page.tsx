0a0646fd1e2561026632850741c6818743433dba
poojanakhale1810 <poojanakhale1810@gmail.com>
2024-09-09 11:22:45 +0530

    add page

--- app/homepage/loginaboutcollage/page.tsx
+++ app/homepage/loginaboutcollage/page.tsx
@@ -1,10 +1,13 @@
+"use client"
 import React from 'react'
 
 export default function Layout({children}) {
   return (
     <div>
+        <ul>
+            <li><Link href="/homepage/loginaboutcollage"></Link></li>
+        </ul>
 
-        
     </div>
   )
 }
